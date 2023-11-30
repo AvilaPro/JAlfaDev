8cbaf708d47ea46c0587ab447597e99e938d3554
avilapro <mejiasavilaeduardo@gmail.com>
2023-11-30 10:59:40 -0400

    clase 2 - JSON y localStorage

--- js/main.js
+++ js/main.js
@@ -65,12 +65,17 @@ class Player {
 
 class Game {
     constructor(name) {
-        this.levels = 3;
-        this.questions = [];
+        this.levels = 3;   
         this.time = 0;
         //agregamos dentro del juego un atributo que direccione al jugador
         this.player = new Player(name);
-        this.chargeQuestions();
+        // this.chargeQuestions();
+        if(localStorage.getItem("game")!== null){
+            let auxLocal = JSON.parse(localStorage.getItem("game"));
+            this.questions = auxLocal.questions;
+        }else{
+            this.questions = preguntas;
+        }
     }
 
     chargeQuestions(){
@@ -104,11 +109,84 @@ class Question {
 }
 
 /**
- * Instanciar un objeto de la clase Game
+ * Declaracion de preguntas
  */
-function startGame() {
-    play = new Game(prompt("Ingresa tu nombre: "));
+var preguntas = [
+    {
+        question: "Cual es la capital de España?",
+        answers: [
+            "Madrid",
+            "Barcelona",
+            "Sevilla",
+            "Valencia"
+        ],
+        rightAnswer: 0
+    },
+    {
+        question: "¿Quién es el autor de la novela Cien años de soledad?",
+        answers: [
+            "Mario Vargas Llosa",
+            "Jorge Luis Borges",
+            "Gabriel García Márquez",
+            "Pablo Neruda"
+        ],
+        rightAnswer: 2
+    },
+    {
+        question: "¿Cuál es el río más largo de América del Sur?",
+        answers: [
+            "Paraná",
+            "Amazonas",
+            "Orinoco",
+            "Magdalena"
+        ],
+        rightAnswer: 1
+    }
+]
+
+/**
+ * Funciones utilitarias del juego
+ */
+
+function showUsername(name) {
+    let title = document.getElementById("username");
+    title.innerText = `Bienvenido ${name}`;
 }
+function showQuestions(quests) {
+    let container = document.getElementById("questions");
+    for (let i = 0; i < quests.length; i++) {
+        container.children[i].children[0].innerHTML = quests[i].question;
+        for (let j = 0; j < quests[i].answers.length; j++) {
+            container.children[i].children[j+1].innerHTML = quests[i].answers[j];
+        }
+    }
+}
+/**
+ * Instanciar un objeto de la clase Game
+*/
+function startGame(el) {
+    if(localStorage.getItem("game") == null){
+        play = new Game(prompt("Ingresa tu nombre: "));
+        localStorage.setItem("game", JSON.stringify(play));
+        showQuestions(play.questions);
+        showUsername(play.player.name);
+        document.cookie = `username=${play.player.name}`;
+    }else{
+        let auxLocal = localStorage.getItem("game");
+        auxLocal = JSON.parse(auxLocal);
+        document.cookie = `username=${auxLocal.player.name}`;
+        showQuestions(auxLocal.questions);
+    }
+    // document.cookie = `username=${play.player.name}`;
+    document.cookie = document.location;
+    //agregar la logica para mostrar el username consumiendo la cookie
+    console.log(document.cookie);
+    console.log(document.cookie.indexOf("username"));
+
+    // showQuestions(play.questions);
+    el.style.display = "none";
+}
+
 
 
 
