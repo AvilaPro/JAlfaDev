e78c0bcdfcee230a466217a42de1d38975a92897
avilapro <mejiasavilaeduardo@gmail.com>
2023-12-07 10:58:18 -0400

    clase 3 - Asincronismo y Sincronismo

--- js/main.js
+++ js/main.js
@@ -65,20 +65,20 @@ class Player {
 
 class Game {
     constructor(name) {
-        this.levels = 3;   
+        this.levels = 3;
         this.time = 0;
         //agregamos dentro del juego un atributo que direccione al jugador
         this.player = new Player(name);
         // this.chargeQuestions();
-        if(localStorage.getItem("game")!== null){
+        if (localStorage.getItem("game") !== null) {
             let auxLocal = JSON.parse(localStorage.getItem("game"));
             this.questions = auxLocal.questions;
-        }else{
+        } else {
             this.questions = preguntas;
         }
     }
 
-    chargeQuestions(){
+    chargeQuestions() {
         for (let i = 0; i < this.levels; i++) {
             let q1 = new Question(i);
             this.questions.push(q1);
@@ -88,19 +88,19 @@ class Game {
 
 class Question {
     constructor(index) {
-        this.question = prompt(`Ingresa la ${index+1}° pregunta : `);
+        this.question = prompt(`Ingresa la ${index + 1}° pregunta : `);
         this.answers = this.createAnswers();
         this.rightAnswer = this.knowRightAnswer();
     }
-    createAnswers(){
+    createAnswers() {
         let answers = [];
-        for (let i=0; i<4; i++){
-            let aux = prompt(`Ingrese la ${i+1}° respuesta: `);
+        for (let i = 0; i < 4; i++) {
+            let aux = prompt(`Ingrese la ${i + 1}° respuesta: `);
             answers.push(aux);
         }
         return answers;
     }
-    knowRightAnswer(){
+    knowRightAnswer() {
         let aux = prompt("Cual es la opcion correcta?");
         aux = parseInt(aux);
         aux--;
@@ -157,21 +157,22 @@ function showQuestions(quests) {
     for (let i = 0; i < quests.length; i++) {
         container.children[i].children[0].innerHTML = quests[i].question;
         for (let j = 0; j < quests[i].answers.length; j++) {
-            container.children[i].children[j+1].innerHTML = quests[i].answers[j];
+            container.children[i].children[j + 1].innerHTML = quests[i].answers[j];
         }
     }
+    contadorTiempo();
 }
 /**
  * Instanciar un objeto de la clase Game
 */
 function startGame(el) {
-    if(localStorage.getItem("game") == null){
+    if (localStorage.getItem("game") == null) {
         play = new Game(prompt("Ingresa tu nombre: "));
         localStorage.setItem("game", JSON.stringify(play));
         showQuestions(play.questions);
         showUsername(play.player.name);
         document.cookie = `username=${play.player.name}`;
-    }else{
+    } else {
         let auxLocal = localStorage.getItem("game");
         auxLocal = JSON.parse(auxLocal);
         document.cookie = `username=${auxLocal.player.name}`;
@@ -185,8 +186,113 @@ function startGame(el) {
 
     // showQuestions(play.questions);
     el.style.display = "none";
+    document.getElementById("btnFinJuego").style.display = "block";
 }
 
+/**
+ * Clase 3 - Asincronismo y Sincronismo
+ */
+//Referencia del banner publicitario
+let banner = document.getElementById("banner");
+setTimeout(() => {
+    banner.style.display = "flex";
+}, 3000);
+banner.addEventListener("click", () => {
+    Swal.fire({
+        title: "Quieres cerrar el banner?",
+        text: "Aprovecha esta oferta!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, cerrar.",
+        cancelButtonText: "No, deseo aprovechar la oferta."
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Cerrado",
+                text: "Oferta eliminada!.",
+                icon: "success"
+            }).then(() => {
+                banner.style.display = "none";
+            });
+        }
+    });
+})
+
+//variable global del cierre de sesion
+var timer;
+var askSess;
+var tiempoJugador = 0;
+var countTimerPlayer;
+//metodo para cerrar sesion
+function timeSession() {
+    timer = window.setTimeout(() => {
+        let timerInterval;
+        Swal.fire({
+            title: "Sesion Cerrada!",
+            html: "Se cerrará automaticamente en 2 segundos...",
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: () => {
+                Swal.showLoading();
+                const timer = Swal.getPopup().querySelector("b");
+                timerInterval = setInterval(() => {
+                    timer.textContent = `${Swal.getTimerLeft()}`;
+                }, 100);
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            }
+        }).then((result) => {
+            /* Read more about handling dismissals below */
+            if (result.dismiss === Swal.DismissReason.timer) {
+                console.log("I was closed by the timer");
+            }
+        });
+    }, 20000);
+}
+
+//funcion para impedir el cierre de la sesion
+function askSession() {
+    askSess = window.setTimeout(() => {
+        Swal.fire({
+            title: "Deseas mantener la sesion?",
+            text: "",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, mantenla!",
+            cancelButtonText: "No, cierrala!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              clearTimeout(timer);
+            }
+          });
+    }, 10000);
+}
+
+//Programamos el DOMContentLoaded para llamar a ambos metodos de Sesion
+addEventListener("DOMContentLoaded", () => {
+    timeSession();
+    askSession();
+})
+
+//Elemento que muestra el contador
+const mostradorConteo = document.getElementById("tiempo");
+//Programar el contabilizador del tiempo
+function contadorTiempo() {
+    countTimerPlayer = window.setInterval(() => {
+        tiempoJugador++;
+        mostradorConteo.innerText = `Tiempo de ejecucion: ${tiempoJugador}`;
+    }, 1000)
+}
+
+function quitarContadorTiempo(el) {
+    clearInterval(countTimerPlayer);
+    el.style.display = "none";
+}
 
 
 
