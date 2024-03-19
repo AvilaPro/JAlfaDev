c8171a52f555fedfce2ea9007063a55d9cc10b30
avilapro <mejiasavilaeduardo@gmail.com>
2024-03-19 11:01:46 -0400

    clase 1

--- js/main.js
+++ js/main.js
@@ -17,3 +17,69 @@ function deleteChild(e) {
         e.parentNode.removeChild(e);
     }
 }
+
+/**
+ * Capitulo 2 Manipulacion  de lista
+ */
+var lista =  document.getElementById("lista");
+//Creacion de los elementos
+document.getElementById("agregar").addEventListener("click", () => {
+    if(document.getElementById("dato").value != ""){
+        let auxLi = document.createElement("li");
+        let auxTextLi = document.createTextNode(document.getElementById("dato").value);
+        //agregar texto al li
+        auxLi.appendChild(auxTextLi);
+        //agregar el li a la lista
+        lista.appendChild(auxLi);
+        //limpíar el input
+        document.getElementById("dato").value = "";
+        auxLi.onclick = function () {
+            if (confirm("Deseas eliminar este elementos?")) {
+                this.parentNode.removeChild(this);
+            }
+        }
+    }
+})
+//Limpieza de la lista
+document.getElementById("limpiar").addEventListener("click", () => {
+    if (confirm("Deseas limpiar la lista?")) {
+        while (lista.childNodes.length > 0) {
+            lista.removeChild(lista.childNodes[0]);
+        }
+    }
+})
+
+/**
+ * Capitulo 3 - Agregar elementos a una tabla
+*/
+var formUser = document.getElementById("formUser");
+var tabla = document.getElementById("tabla");
+//Obtener los datos del usuario y agregarlos a la tabla
+document.getElementById("addUser").addEventListener("click", () =>{
+    let auxtr = document.createElement("tr");
+    let arrData = [];
+    arrData.push(document.getElementById("nombre").value);
+    arrData.push(document.getElementById("telefono").value);
+    arrData.push(document.getElementById("correo").value);
+    console.log(arrData);
+    formUser.reset();
+    for (const d of arrData) {
+        console.log(d);
+        let tdAux = document.createElement("td");
+        let auxTextTd = document.createTextNode(d);
+        tdAux.appendChild(auxTextTd);
+        auxtr.appendChild(tdAux);
+    }
+    let btnRow = auxtr.insertCell(-1);
+    btnRow.innerHTML = "<button class='btn btn-warning' onclick='deleteRow(this, event)'>Eliminar</button>"
+    tabla.tBodies[0].appendChild(auxtr);
+})
+//Metodo para eliminar una fila de la tabla
+function deleteRow(e, ev) {
+    ev.stopPropagation();
+    if(confirm("Deseas eliminar el usuario?")){
+        fila = e.parentNode.parentNode.rowIndex;
+        console.log(fila);
+        tabla.deleteRow(fila);
+    }
+}
