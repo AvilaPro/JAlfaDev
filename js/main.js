0f0ee7c2194141635bed40ae20088f3ed5172101
avilapro <mejiasavilaeduardo@gmail.com>
2024-03-26 10:54:04 -0400

    clase 2 validacion de formularios y expresiones regulares

--- js/main.js
+++ js/main.js
@@ -83,3 +83,102 @@ function deleteRow(e, ev) {
         tabla.deleteRow(fila);
     }
 }
+
+
+/**
+ * **
+ * *
+ * *
+ * *
+ * 
+ * Clase 2 - Validacion de formulario
+ */
+
+/**
+ * Cap 5 -  Manejo del submit
+ */
+function driverSubmit(e) {
+    e.preventDefault();
+    let f = document.loginForm;
+    let values = []
+    console.log(f.elements);
+    for (const input of f.elements) {
+       if (input.type == "text" || input.type == "password") {
+        values.push(input.id + ": " + input.value+ ",");
+       } 
+    }
+    console.log("en el submit");
+    console.log(values);
+}
+
+function driverRegister() {
+    let f = document.registerForm;
+    f.submit();
+}
+
+/**
+ * Cap 6 - Validacion de inputs
+ */
+function validarInputs(f) {
+    let typesValidos = ["text", "email", "password"];
+    for (const input of f.elements) {
+        if (typesValidos.includes(input.type)) {
+            if (input.value =="") {
+                alert(`El input ${input.type}, se encuentra vacio, debe suministrar un valor`);
+                input.focus();
+                return false;
+            }
+        }
+        if (input.type == "select-one") {
+            if (input.selectedIndex == 0) {
+                alert("El select debe tener una seleccion");
+                input.focus();
+                return false
+            }
+        }
+        if (input.type == "checkbox" && input.required ) {
+            if (!input.checked) {
+                alert (`Debe marcar la casilla de las políticas`);
+                input.focus();
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function validarNombre(input) {
+    let regexpName = /^([A-Za-zñÑáéíóúÁÉÍÓÚ])([A-Za-zñÑáéíóúÁÉÍÓÚ0-9 ]{0,19})$/;
+    if (regexpName.test(input.value)) {
+        return true;
+    }else{
+        alert("El nombre suministrado no es del formato esperado, no puede iniciar con numero, no puede tener caracteres especiales y debe tener una longitud maxima de 20 caracteres");
+        input.focus();
+        return false;
+    }
+}
+
+function validarForm(btn) {
+    // console.log(btn);
+    if (btn.type == "button") {
+        if (validarInputs(btn.form)) {
+            // btn.form.submit();
+            console.log("FORMULARIO CON CAMPOS RELLENOS");
+            if (validarNombre(btn.form.name)) {
+                console.log("INPUT DEL NOMBRE VALIDO");
+            }else{
+                console.log("INPUT DEL NOMBRE INVALIDO");
+            }
+        }else{
+            console.log("FORMULARIO CON ALGUN CAMPO VACIO");
+        }
+    }
+    if (btn.tagName ==  "FORM") {
+       if (validarInputs(btn)) {
+        return true
+       }else{
+        console.log("FORMULARIO INVALIDO");
+        return false
+       }
+    }
+}
