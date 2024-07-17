b25f102969dcf278913f7abe483e21d9c5fc9001
avilapro <mejiasavilaeduardo@gmail.com>
2024-07-17 17:10:13 -0400

    clase 4 - Efectos con jQuery

--- js/effects.js
+++ js/effects.js
@@ -1,3 +1,42 @@
+let personajes;
+fetch("https://rickandmortyapi.com/api/character").
+then(res => res.json()).
+then(data => {
+  console.log(data.results);
+  personajes = data.results;
+  for (const personaje of personajes) {
+    $('#personajes').append(`
+      <div class="card col-3">
+      <img src="${personaje.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${personaje.name}</h5>
+        <p class="card-text">Specie: ${personaje.species}, Status: ${personaje.status}</p>
+        <button class="btn btn-primary" disabled>${personaje.location.name}</button>
+        <button class='btn btn-danger' onclick='eliminarCard(this)'> Eliminar </button>
+        <button class='btn btn-warning' onclick='ocultarCard(this)'> Ocultar </button>
+      </div>
+      </div>  
+    `)
+  }
+})
+
+function eliminarCard(el) {
+  $(el.parentNode.parentNode).remove();
+}
+function ocultarCard(el) {
+  $(el.parentNode.parentNode).
+  animate({
+    opacity: 0.2,
+    top: '-50px',
+    position: 'absolute',
+    right: '-100px'
+  }, 2000).
+  css({
+    'background': 'green'
+  }).
+  slideUp(2000);
+}
+
 $(document).ready(() => {
   $('#jqueryAlfa .btn-primary').click(() => {
     $('#jqueryAlfa .btn-success').toggle();
@@ -64,4 +103,40 @@ $(document).ready(() => {
     }).removeClass('disabled');
     $('#liLogout').hide();
   })
+
+  /**
+   * Probando agregar y eliminar contenido con jQuery
+   */
+  //probar el append
+  $('.appendBtn').click(() => {
+    $("#addDeleteJQuery").append('contenido append')
+  })
+  //probar el prepend
+  $('.prependBtn').click(() => {
+    $("#addDeleteJQuery").prepend('contenido prepend')
+  })
+  //probar el after
+  $('.afterBtn').click(() => {
+    $("#addDeleteJQuery").after('<p>contenido after</p>')
+  })
+  //probar el before
+  $('.beforeBtn').click(() => {
+    $("#addDeleteJQuery").before('<p>contenido before</p>')
+  })
+  //probar el empty
+  $('.emptyBtn').click(() => {
+    $("#addDeleteJQuery").empty();
+  })
+  //probar el remover
+  $('.removeBtn').click(() => {
+    $("#addDeleteJQuery").remove();
+  })
+  /**
+   * Probar animate con scroll
+   */
+  $('#addDeleteJQuery').fadeOut();
+  $(window).on("scroll", function() {
+    $('#addDeleteJQuery').fadeIn(3500);
+  });
 });
+
