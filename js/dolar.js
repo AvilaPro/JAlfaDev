5322c00b54f99681642a846e518a9b178f2c71e5
avilapro <mejiasavilaeduardo@gmail.com>
2024-10-08 08:39:27 -0400

    preliminar de pagina de conversion dolar

---
+++ js/dolar.js
@@ -0,0 +1,15 @@
+
+fetch('https://ve.dolarapi.com/v1/dolares').
+then(res => res.json()).
+then(data => {
+    console.log(data);
+    //Codigo para actualizar el select del formulario con los datos obtenidos del API
+    //Option BCV
+    document.getElementById('BCV').innerHTML += data[0].promedio;
+    document.getElementById('BCV').value = data[0].promedio;
+    //Option Paralelo
+})
+
+function calcular() {
+    
+}
