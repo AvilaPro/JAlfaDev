d87ab28534d88b6f2f45fe19d10abe63e5060e4f
avilapro <mejiasavilaeduardo@gmail.com>
2025-03-19 18:21:36 -0400

    Clase 4 culminada 	modified:   clase4.html 	modified:   js/clase4.js

--- js/clase4.js
+++ js/clase4.js
@@ -1,8 +1,12 @@
 var products;
+var filteredProducts;
+var categories = [];
+
+let lista = document.getElementById("lista");
+let categorias = document.getElementById("categorias");
 
 //Manejo del renderizado de elementos en la pagina web
 window.addEventListener("load", () => {
-  let lista = document.getElementById("lista");
   fetch('https://fakestoreapi.com/products')
     .then(response => response.json())
     .then((data) => {
@@ -20,9 +24,52 @@ window.addEventListener("load", () => {
                         <span style="color: lightblue">${product.category}</span>
                         <a href="#" class="btn btn-primary buttonPrice">Comprar ${product.price}$</a>
                       </div>
-                    </div>
-                  `
-
+                    </div>                  `
       }
-    })
+      //Determinar las categorias de los productos.
+      products.forEach((p) => {
+        if (!categories.some((c) => c == p.category)) {
+          categories.push(p.category);
+        }
+      });
+      console.log(categories);
+      for (const cat of categories) {
+        categorias.innerHTML += `<option value="${cat}">${cat}</option>`;
+      }
+  });
 });
+
+categorias.onchange = function () {
+  console.log(categorias);
+  if (categorias.value != '') {
+    filteredProducts = products.filter((p) => p.category == categorias.value);
+    //Resetear la lista de productos
+    lista.innerHTML = '';
+    for (const product of filteredProducts) {
+      lista.innerHTML += `
+                  <div class="card">
+                    <img src="${product.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${product.title}</h5>
+                      <p class="card-text product-description text-truncate">${product.description}</p>
+                      <span style="color: lightblue">${product.category}</span>
+                      <a href="#" class="btn btn-primary buttonPrice">Comprar ${product.price}$</a>
+                    </div>
+                  </div>                  `
+    }
+  }else{
+    lista.innerHTML = '';
+    for (const product of products) {
+      lista.innerHTML += `
+                  <div class="card">
+                    <img src="${product.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${product.title}</h5>
+                      <p class="card-text product-description text-truncate">${product.description}</p>
+                      <span style="color: lightblue">${product.category}</span>
+                      <a href="#" class="btn btn-primary buttonPrice">Comprar ${product.price}$</a>
+                    </div>
+                  </div>                  `
+    }
+  }
+}
